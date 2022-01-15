2521fd87c571747d4bf11284a70f64b94eb3f29c
Samantha Diaz <samidiaz3@gmail.com>
2022-01-14 19:09:34 -0500

    Done.

--- index.js
+++ index.js
@@ -10,4 +10,24 @@ function handleClick(e) {
 /* End Given Code, don't edit above here...*/
 
 /* Write your implementation of greet() */
+  // Examples:
+  // timeMorning = "09:00"
+  // timeAfternoon = "12:00"
+  // timeEvening = "18:00"
+
+function greet(timeString){
+  const hour = parseInt(timeString, 10);
+  
+  if (hour < 12) return `Good Morning`
+  if (hour > 17) return `Good Evening`
+  return `Good Afternoon`
+}
+
 /* Write your implementation of displayMessage() */
+function displayMessage(timeString){
+  let element = document.querySelector("#greeting")
+  element.innerHTML = timeString
+
+  // GIVEN SOLUTION:
+  // document.getElementById("greeting").textContent = timeString
+}
